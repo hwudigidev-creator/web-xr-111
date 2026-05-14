118df917317f9cca0071677b34988abe59659f88
CodingCatz <CodingCatz@users.noreply.github.com>
2026-05-14 17:15:37 +0000

    deploy: b482baa6c44239314bfc4d69570e4c676a4678d0

---
+++ sw.js
@@ -0,0 +1,55 @@
+const CACHE_NAME = 'error-ar-v1';
+const CORE_ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/error-icon.svg',
+  './icons/error-icon-192.png',
+  './icons/error-icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request;
+
+  if (request.method !== 'GET' || new URL(request.url).origin !== self.location.origin) {
+    return;
+  }
+
+  event.respondWith(
+    fetch(request)
+      .then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(request, clone));
+        return response;
+      })
+      .catch(() =>
+        caches.match(request).then((cached) => {
+          if (cached) {
+            return cached;
+          }
+
+          if (request.mode === 'navigate') {
+            return caches.match('./index.html');
+          }
+
+          return Response.error();
+        })
+      )
+  );
+});
